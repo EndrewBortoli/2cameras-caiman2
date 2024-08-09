2a9d2cad7b3780947c1fad31b3c49dd3be82c822
EndrewBortoli <endrew.ryan977@gmail.com>
2024-08-09 16:52:49 -0300

    A

--- UpdateCom2Cameras/src/main/java/frc/robot/Constants.java
+++ UpdateCom2Cameras/src/main/java/frc/robot/Constants.java
@@ -32,7 +32,7 @@ public final class Constants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
     
-    public static final double kMaxSpeedMetersPerSecond = 2;
+    public static final double kMaxSpeedMetersPerSecond = 3;
     public static final double kMaxAngularSpeed = 1.5 * Math.PI; // radians per second
 
     public static final double kDirectionSlewRate = 6; // radians per second
